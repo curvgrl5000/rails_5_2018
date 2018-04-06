d96a57e195ab6bf33fb1baa868628ac0ebee339e
Anna Mendoza <annamendoza.curve@gmail.com>
2018-04-05 22:28:55 -0700

    added the new static_pages with about.html.erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render html: "hello!"
-	end
+		text = [ "trapped at the root application controller!", "I'm trapped!"]
 
+    render html: text[0]
+	end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-  	render html: "hey I'm home"
+  	 # render html: "hey I'm home"
   end
 
   def help
-  	render html: "hey I need help!"
+  	# render html: "hey I need help!"
   end
+
+  def about
+  	# render html: "hey I need help!"
+  end
+  
 end
