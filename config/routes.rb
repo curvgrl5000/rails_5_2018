17fe7d48ccf069b13db2b13f3358ca94ce7c1331
Anna Mendoza <annamendoza.curve@gmail.com>
2018-06-28 20:55:29 -0700

    added the crud for the sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/index'
 
   get 'users/show'
@@ -21,6 +23,12 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post '/signup', to: 'users#create'
+
+  get  '/login',   to: 'sessions#new'
+  
+  post  '/login',   to: 'sessions#create'
+  
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 
