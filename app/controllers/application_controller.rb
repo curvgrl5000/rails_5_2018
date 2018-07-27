907422bb94559be91dee2fa6914f62e5b51a304d
Anna Mendoza <annamendoza.curve@gmail.com>
2018-07-26 19:41:07 -0700

    added to the helper pages all the session functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-	def hello
-		text = [ "trapped at the root application controller!", "I'm trapped!"]
-
-    render html: text[0]
-	end
+	include SessionsHelper
 end
